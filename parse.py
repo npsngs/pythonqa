e3d488901ffa452ee6b702ed851fe7a63ed0bb0b
hecc <npsngs123@163.com>
2016-06-23 17:26:16 +0800

    modify

--- parse.py
+++ parse.py
@@ -3,13 +3,46 @@
 dictFile = open('中文字典.txt','r+')
 outFile = open('chinesec.txt', 'w+') 
 ifPause = True
+lineOut = u''
+
+def onBegin(lineOut):
+	global outFile
+	if len(lineOut) > 0:
+		lineOut +='\n'
+		#print lineOut
+		outFile.write(lineOut.encode('utf-8'))
+		lineOut = u''
+
+
+
+
+def parseDict(lineStr):
+	global lineOut
+	if lineStr.startswith('*'):
+		onBegin(lineOut)
+		lineOut = lineStr[1:2] + '\t'
+		idx = lineStr.find(',')
+		if idx >= 0:
+			lineOut += lineStr[2:idx]
+
+
+
+
+
+
+
 while True:
 	line = dictFile.readline()
-	print line
+	line = line.decode('utf-8')
+	if ifPause:
+		print line
+		print ':',
+
 	if ('' == line):
 		break
-	print ':',
-
+	else:
+		parseDict(line)
+	
 	if not ifPause:
 		continue
 
@@ -20,11 +53,7 @@ while True:
 		ifPause = False
 		continue
 
-
-def parseDict(lineStr):
-
-
-	
+outFile.flush()	
 
 
 
